ed692f28ba026094b838888043368387c2b931d9
vivek <vivekrandhawa6@gmail.com>
2025-06-10 13:20:44 +0530

    bug fix

--- server/app.js
+++ server/app.js
@@ -11,7 +11,8 @@ const IdeaRoute = require("./Routes/Idea.js")
 //essential middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(cookieParser());
+// app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(
   CORS({
     origin: "https://aithink-idea-aggregator-client.onrender.com",
